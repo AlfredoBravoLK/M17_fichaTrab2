4a1d359ff4912ae467eadc84d0b937a64d44d6a9
a18984 <a18984@d1-08-08.dped.local>
2017-02-07 16:23:42 +0000

    Versão beta 0.1

--- src/m17b_fichatrab2/Ponto2D.java
+++ src/m17b_fichatrab2/Ponto2D.java
@@ -6,13 +6,33 @@ public class Ponto2D {
     
     // Faltam construtor, getters e setters.
     
+    public Ponto2D(){
+        this.x = 0;
+        this.y = 0;        
+    }
+    
+    /**
+     * 
+     * @param x
+     * @param y 
+     */
+    
     public Ponto2D(int x, int y){
         this.x = x;
         this.y = y;        
     }
     
+    /**
+     * Calcular o valor entre as distâncias entre os dois pontos.
+     * @param a
+     * @return Distância entre ambos os pontos.
+     */
+    
     public double distancia(Ponto2D a){
         // Falta fazer este método.
+        
+        
+        
         return 0.0;
     }
     
