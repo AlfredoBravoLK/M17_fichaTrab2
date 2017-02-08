b5716418e169bce42bf4495ef2f881af2cbe68c6
a18984 <a18984@d1-08-08.dped.local>
2017-02-08 11:34:39 +0000

    Ficha de trabalho #2

--- src/m17b_fichatrab2/Ponto2D.java
+++ src/m17b_fichatrab2/Ponto2D.java
@@ -1,18 +1,29 @@
 package m17b_fichatrab2;
 
+/**
+ * Classe Ponto2D, para instaciar pontos.
+ * @version 0.1, 8.fev.2017
+ * @author Alfredo Bravo
+ */
+
 public class Ponto2D {
+    /** Coordenada X */
     private int x;
+    /** Coordenada Y */
     private int y;
+
+    /** 
+     * Construtor sem parâmetros, que inicializa os atríbutos
+     * com valores predefenidos.
+     */
     
-    // Faltam construtor, getters e setters.
-    
-    public Ponto2D(){
+    public Ponto2D() {
         this.x = 0;
         this.y = 0;        
     }
     
     /**
-     * 
+     * Contrutor de classe parametrizado com todos os atríbutos.
      * @param x
      * @param y 
      */
@@ -22,6 +33,23 @@ public class Ponto2D {
         this.y = y;        
     }
     
+    public int getX(){
+        return x;
+    }
+    
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    
+    public int getY() {
+        return y;
+    }
+    
+    public void setY(int y) {
+        this.y = y;
+    }
+    
     /**
      * Calcular o valor entre as distâncias entre os dois pontos.
      * @param a
@@ -29,11 +57,17 @@ public class Ponto2D {
      */
     
     public double distancia(Ponto2D a){
-        // Falta fazer este método.
         
-        
-        
-        return 0.0;
+        return Math.abs(Math.sqrt(Math.pow((x-a.x),2)+Math.pow((y-a.y),2)));
+    }
+    
+    /**
+     * @return Uma String composta pelos atríbutos da classe
+     */
+    
+    @Override
+    public String toString() {
+        return "Ponto2D{" + "x=" + x + ", y=" + y + '}';
     }
     
 }
